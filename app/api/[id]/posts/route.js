3bc5b2558e613bb8c023dbea0b108d04a8627a95
sajtel <sajtel@sfu.ca>
2024-05-10 14:30:32 -0700

    added functionality for profile page

--- app/api/[id]/posts/route.js
+++ app/api/[id]/posts/route.js
@@ -2,13 +2,14 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 
-export const GET = async (request) => {
+export const GET = async (request, {params}) => {
 
     try{
 
         await connectToDB();
+
         
-        const prompts = await Prompt.find({}).populate('creator');
+        const prompts = await Prompt.find({creator:params.id}).populate('creator');
 
         return new Response(JSON.stringify(prompts), {stats: 200}) 
 
